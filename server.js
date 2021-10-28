39c86d2420fe420743f55dc3b77d392c07380d8b
Zhicheng <zhichengzhou123@gmail.com>
2021-10-28 15:35:52 -0700

    testing

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+//const { application } = require('express');
+const bodyparser = require("body-parser");
+const path = require('path');
+
+
+dotenv.config({path: `./config.env`}); // seperate the port number from the main source code
+
+const PORT = process.env.PORT || 3000; //listen on the port number otherwise listen on default 3000 port
+
+//log requests
+app.use(morgan(`tiny`));
+
+//parse request to body parse
+app.use(bodyparser.urlencoded({extended: true}));
+
+//set view engine   
+app.set("view engine", "ejs");
+//app.engine('ejs', require('ejs'));
+//app.set("views", path.resolve(__dirname, "views/ejs"));
+
+//loaad assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")));
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
+
+app.get('/', (req, res) => {
+    res.render(`index`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+});
+
